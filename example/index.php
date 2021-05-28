2b9e39b297255977c0a188aa2b1f3c5dcdd669cf
Ismael Fermandez <ismael.7087@gmail.com>
2021-05-28 12:55:41 -0500

    initial commit

---
+++ example/index.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . "/../vendor/autoload.php";
+
+$document = new \tech\facturador\Document();
+$document->create();
---
+++ src/Document.php
@@ -0,0 +1,9 @@
+<?php
+namespace tech\facturador;
+class Document
+{
+    public function create()
+    {
+        echo "create a document";
+    }
+}
