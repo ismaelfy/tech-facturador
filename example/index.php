5e87e1d9842463f5f0304373eb155d627941af3f
Ismael Fermandez <ismael.7087@gmail.com>
2021-05-28 13:10:14 -0500

    initial commit

--- example/index.php
+++ example/index.php
@@ -1,5 +1,5 @@
 <?php
 include __DIR__ . "/../vendor/autoload.php";
-
-$document = new \tech\facturador\Document();
+use \tech\facturador\Document;
+$document = new Document();
 $document->create();
